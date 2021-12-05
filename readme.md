82167f09dee2e4d8daa2fed99c6c59cec507dc08
EricThomson <thomson.eric@gmail.com>
2021-12-04 22:36:18 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# tiffit
+command-line interface for multi-page tiff conversion, concatenation. and metadata extraction. That's it. Nothing fancy.
+
+Lighgtweight wrapper for tifftools:
+https://github.com/DigitalSlideArchive/tifftools.
+
+tiffit write x.tiff y.tiff --bigtiff --bigendian
+tiffit concat x.tiff y.tiff -bigtiff --bigendian
+
+## installation
+Currently no installer
+
+    conda create -n tiffit python=3.8
+    pip install tifftools
+    conda install -c conda-forge matplotlib
+
+If you want to use spyder, follow this:
+https://github.com/spyder-ide/spyder/wiki/Working-with-packages-and-environments-in-Spyder
---
+++ tiffit_workspace.py
@@ -0,0 +1,142 @@
+"""
+Figuring out multipage tiff basics
+
+Using tifffile package
+"""
+import tifffile  # to use imread and imwrite
+from tifffile import TiffFile
+import os
+import matplotlib.pyplot as plt
+
+# %%
+import tifftools
+
+# %%
+"""
+seems 0 (idps) is inscopix (idp inscopix data project the s stands for wtf) --
+    this is NOT imagej but inscopix, this is what I use.
+seems 1 is bioformat saved using imagej
+seems 2 is I have no fucking idea what.
+
+Not sure what the problem is exactly. Basically mesmerize won't open in inscopix.
+It *is* trying to save as imagej and bigtiff at the same time, which is a no-no they are literally
+incompatible file types. Just pick one I guess?
+"""
+working_dir = r'C:/Users/Eric/Dropbox/Programming/tiffit/'
+data_dir = working_dir + r'data/'
+data_files = os.listdir(data_dir)
+for data_file in data_files:
+    print(data_file)
+
+# %%
+
+filename = data_files[1]
+file_path = data_dir + filename
+print(file_path)
+# %%
+tifftools.tiff_dump(data_dir + data_files[0])
+tifftools.tiff_dump(data_files[1])
+tifftools.tiff_dump(data_files[2])
+
+# %%
+"""
+What I need to do is pretty simple:
+    try out different permutations of saving bullshit between mesmerize and inscopix.
+    1. how to load data into inscopix (data that actually works)
+    2. Run some shit in mesmerize (small data) with mc
+    3. Try to load in inscopix
+    4. Fix it.
+
+    First need to port all this shit over to the beast.
+"""
+
+# %%
+"""
+Below is tifftools stuff
+
+For tiff tags:
+    https://github.com/DigitalSlideArchive/tifftools/blob/master/tifftools/constants.py#L522
+"""
+info = tifftools.read_tiff(file_path)
+tags = info['ifds'][0]['tags']
+for key in tags.keys():
+    print(key)
+
+# %% for image 0 (same for image 1)
+256
+257
+258
+259
+262
+270  # in image 1 but not 0
+273
+274  # 0 but not 1
+277
+278
+279
+282  # 1 but not 0  xresolution
+283  # 1 but not 0 yresolution
+284  # planarconfig
+296  # resolution limit
+305  # 1 not 0: software
+339
+
+
+# %% for image 2
+254  # : newsubfile type (useless)
+256  # imagewidth
+257  # imagelength
+258  # bits per sample
+262  # photometric ? (color space of the data)
+270  # image description (this can be really useful)
+273  # strip offsets (I will never use this)
+# samples per pixel : number of components per pixel (what is a component?)
+277
+278  # rows per strip
+279  # strip byte counts
+50838  # imageJ custom bullshit
+50839  # imageJ custom bullshit
+
+# %%
+tags[256]
+tags[262]
+tags[tifftools.Tag.
+
+# %%
+tags[tifftools.Tag.NewSubFileType.value]  # this is useless
+tags[tifftools.Tag.ImageDescription.value]
+tags[tifftools.Tag.ImageHeight.value]
+tags[tifftools.Tag.ImageWidth.value]
+
+tags[tifftools.Tag.Photometric.value]
+tags[262]
+
+tags[50838]
+
+exififd = info['ifds'][0]['tags']
+exififd.keys()
+
+# %% ifd = image file directory
+"""
+Below is tifffile stuff
+"""
+
+# %%
+
+# %%
+tf = TiffFile(file_path)  # as tf:
+print(f"Byte order: {tf.byteorder}")
+num_images = len(tf.pages)
+print(f"Num images: {num_images}")
+first_ifd = tf.pages[0]
+print(first_ifd.shape)
+page_tags = first_ifd.tags
+
+print(tf.is_imagej)
+print(tf.is_bigtiff)
+# %%
+
+
+# %%
+image_data = first_ifd.asarray()
+plt.imshow(image_data, cmap='gray')
